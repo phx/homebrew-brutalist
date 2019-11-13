dab809f616e105af598bf09288b4b1caa7dba248
phx <oldjamey@gmail.com>
2019-11-13 14:18:35 -0600

    removed stray quote

--- brutalist.rb
+++ brutalist.rb
@@ -1,5 +1,5 @@
 class HomebrewBrutalist < Formula
-  desc "brutalist is a python3-based command-line tool for all platforms that can be used to generate quick and large word lists from 1 or more sample passwords provided by the user.""
+  desc "brutalist is a python3-based command-line tool for all platforms that can be used to generate quick and large word lists from 1 or more sample passwords provided by the user."
   homepage "https://github.com/phx/brutalist"
   url "https://github.com/phx/homebrew-brutalist/archive/1.0.tar.gz"
   sha256 "8bbebcd863cebaccae153a2ee114ee052bef6b7b7f54bab562a5b6b9ffcf96f5"
