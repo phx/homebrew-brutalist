371184fedae3b1d0c304e111566fe7d51dc25042
phx <oldjamey@gmail.com>
2019-11-13 14:29:45 -0600

    homebrew fix

--- brutalist.rb
+++ brutalist.rb
@@ -4,12 +4,12 @@ class HomebrewBrutalist < Formula
   url "https://github.com/phx/homebrew-brutalist/archive/1.0.tar.gz"
   sha256 "8bbebcd863cebaccae153a2ee114ee052bef6b7b7f54bab562a5b6b9ffcf96f5"
 
-  depends_on "python3" => 3.0
+  depends_on :python3 => 3.0
 
   def install
       # move 'brutalist.py' under #{prefix}/bin/
       bin install "brutalist.py"
-      mv bin/brutalist.py bin/brutalist
+      mv bin/"brutalist.py" bin/"brutalist"
   end
 
   test do
