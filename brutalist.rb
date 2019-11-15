0606b7df767dc94bf2c316cde3c233927a7356a1
oldjamey <oldjamey@icloud.com>
2019-11-15 12:08:09 -0600

    upgrading homebrew formula to 1.5 release

--- brutalist.rb
+++ brutalist.rb
@@ -1,8 +1,8 @@
 class Brutalist < Formula
   desc "brutalist is a python3-based command-line tool for all platforms that can be used to generate quick and large word lists from 1 or more sample passwords provided by the user."
   homepage "https://github.com/phx/brutalist"
-  url "https://github.com/phx/homebrew-brutalist/archive/1.0.4.tar.gz"
-  sha256 "466b0b9dced3ff434bb6990c919ff4934199b4df98635fb225a6ff08c6a9ffc0"
+  url "https://github.com/phx/homebrew-brutalist/archive/1.5.0.tar.gz"
+  sha256 "ac1f34b5873bddbb2b71c0b353867f6addbbda91ccf0fddc8057af89db66eb64"
 
   depends_on "python" => "3.0"
 
