a5ed2885f65af371f615738ec762a77957b17fe5
phx <oldjamey@gmail.com>
2019-11-30 20:33:32 -0600

    updated to version 1.6.9

--- brutalist.rb
+++ brutalist.rb
@@ -1,8 +1,8 @@
 class Brutalist < Formula
   desc "brutalist is a python3-based command-line tool for all platforms that can be used to generate quick and large word lists from 1 or more sample passwords provided by the user."
   homepage "https://github.com/phx/brutalist"
-  url "https://github.com/phx/homebrew-brutalist/archive/1.6.7.tar.gz"
-  sha256 "f1b9d933ce79d6cf7c9a7fd61028cafba7717ab259cf164b622766b7dc99437a"
+  url "https://github.com/phx/homebrew-brutalist/archive/1.6.9.tar.gz"
+  sha256 "21088c2a67d609f836800fc8451dcc12a02ebd38aa36af3526304e1fe7d1874c"
 
   depends_on "python" => "3.0"
 
